43d2a1def92069d0847586d7d5703bd98a433493
ds1910 <dineshsharma7429@gmail.com>
2025-06-10 23:51:41 +0530

    Starting New Project

---
+++ index.js
@@ -0,0 +1,38 @@
+// Essential requires
+const express = require("express");
+require('dotenv').config();
+
+
+
+// Connection and middleware requires
+const connectMongoDb = require("./connection");
+const { logReqRes } = require("./middleware");
+
+// App initialization
+const app = express();
+const port = process.env.PORT;
+
+// Routers
+
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logReqRes("log.txt"));
+
+
+// Connect to MongoDB
+connectMongoDb("mongodb://127.0.0.1:27017/BugVault").then(() => {
+  console.log("MongoDB connected");
+});
+
+// EJS setup for server-side rendering
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+
+// Routes
+
+
+// Start server
+app.listen(port, () => console.log("Server started"));
